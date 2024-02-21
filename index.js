104276fa5ba46dde3c790a604827c00a9393852f
sujit <kumsuj5>
2024-02-21 17:04:55 +0530

    this is back code

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-
-// Load environment variables from .env file
-dotenv.config();
-
 const port = process.env.PORT || 3000;
-const mongoURI = process.env.MONGODB_URI;
-
 const app = express();
 
-mongoose.connect(mongoURI, {  useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000 });
-
+mongoose.connect('mongodb+srv://kumsuj5:8953729002@restapi.d5hd8is.mongodb.net/?retryWrites=true&w=majority');
 mongoose.connection.on('error', err => {
-    console.log("Connection to MongoDB failed:", err);
+    console.log("connection failed mongoose");
 });
-
 mongoose.connection.on('connected', () => {
-    console.log("Connected to MongoDB");
+    console.log("connected to mongoose");
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
 
 const userRoute = require('./routers/user');
 const studentRoute = require('./routers/student');
+
 const faculityRoute = require('./routers/faculity');
 
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/student', studentRoute);
 app.use('/api/v1/faculity', faculityRoute);
 
-app.use('/server', (req, res, next) => {
-    res.status(200).send("Server is working");
-});
+
+app.use('/server',(req,res,next)=>{
+    res.status(200).send("Server is working")
+} )
 
 app.use((req, res, next) => {
     res.status(404).json({
-        error: "Not Found"
+        error: "bad request"
     });
 });
 
 app.listen(port, () => {
-    console.log("Server is running on localhost:" + port);
+    console.log("Server is running on localhost " + port);
 });
